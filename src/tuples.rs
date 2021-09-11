290747e3218cc6ba6ddd0e3c1fe8c58b48e0d06f
Borislav Stoyanov <bostoycontact@gmail.com>
2021-09-11 13:18:15 +0300

    tuples

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,7 +1,11 @@
 //Tuples group together values of different types.
-//Max 12 elements
+//Fixed length: They cant grow or shrink in size
 
-pub fn run(){
-let person: (&str,&str,i8) = ("Borislav", "Stoyanov", 21);
-println!("{} is from {} and is {}", person.0,person.1,person.2);
+pub fn run() {
+    let person: (&str, &str, i8) = ("Borislav", "Stoyanov", 21);
+
+    let (first_name, last_name, age) = person; //destructuring
+
+    println!("{} is from {} and is {}", person.0, person.1, person.2);
+    println!("{} is from {} and is {}", first_name, last_name, age);
 }
