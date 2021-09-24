befa4112f8062ff03b15d74d48000d2d0399a2b3
Borislav Stoyanov <bostoy@protonmail.com>
2021-09-24 14:50:51 +0300

    Enums

---
+++ src/enums.rs
@@ -0,0 +1,28 @@
+pub fn run() {
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+    }
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky Penny");
+                1
+            }
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                 println!("State quarter from {:?}!", state);
+                return 25;
+            },
+        }
+    }
+value_in_cents(Coin::Quarter(UsState::Alaska));
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod enums;
+mod option_enum;
+fn main() {
+    enums::run();
+    option_enum::run();
+}
